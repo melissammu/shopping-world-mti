fe3e1eafe9177990f491f749f7684dfe2c75c76c
Melissa Mejías <melissamejias254@gmail.com>
2026-03-18 19:02:48 -0300

    supabase conectado correctamente

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://TU-PROYECTO.supabase.co";
-const supabaseAnonKey = "TU_ANON_KEY";
+const supabaseUrl = "https://zptdwmsbyrccmcfyttr.supabase.co";
+const supabaseAnonKey = "sb_publishable_AQUI_TU_KEY_REAL";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
