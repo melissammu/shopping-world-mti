9e05587ec82c03fc7d281fd2ee5fa9e7bd246ff6
Melissa Mejías <melissamejias254@gmail.com>
2026-03-20 21:34:29 -0300

    fix ruta sitePage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import OpenBrowserPage from "./pages/OpenBrowserPage";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import SitePage from "./pages/sitePages";
+import SitePage from "./pages/sitePage";
 import AmazonPage from "./pages/amazonPage";
 import AmazonUsaPage from "./pages/amazonUsaPage";
 import SheinPage from "./pages/sheinPage";
@@ -17,7 +17,7 @@ export default function App() {
       <Route path="/login-afiliada" element={<AffiliateLoginPage />} />
       <Route path="/search" element={<SearchPage />} />
       <Route path="/" element={<OpenBrowserPage />} />
-      <Route path="/home" element={<SitePages/>} />
+      <Route path="/home" element={<SitePage/>} />
       <Route path="/amazon" element={<AmazonPage />} />
       <Route path="/amazonusa" element={<AmazonUsaPage />} />
       <Route path="/shein" element={<SheinPage />} />
